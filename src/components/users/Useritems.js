78ee032f8b7060d755688ae0e6a632b9d2e43741
BLACKHAT <mm9636389895@gmail.com>
2022-04-27 01:53:21 +0530

    creating components of user and difference in states

--- src/components/users/Useritems.js
+++ src/components/users/Useritems.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 
 class Useritems extends Component {
-    constructor(){
-        super();
-        this.state={
-            id:'',
-            login:'',
-            avatar_url:'',
-            html_url:''
-        }
-    }
+  state={
+    id:'id',
+    login:'mojombo',
+    avatar_url:'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url:'https://github.com/mojombo'
+}
   render() {
+    const {login , avatar_url ,html_url}=this.state;
     return (
-      <div>Useritems</div>
+      <div className="card text-center"> 
+      <img src={avatar_url} alt=""  className="round-img"style={{width:'60px'}}/>
+      <h3>{login}</h3>
+      <div>
+      <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+      </div>
+      </div>
     )
   }
 }
