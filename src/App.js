21e10448b40b76777c809a8e00616dbd6391ad32
BLACKHAT <mm9636389895@gmail.com>
2022-05-03 09:58:57 +0530

    search component and clear component made

--- src/App.js
+++ src/App.js
@@ -15,20 +15,37 @@ class App extends Component {
      this.setState({loading:true})//setting loading true till we get back our data as per requirement 
 
    const res = await axios
-    .get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    .get(`https://api.github.com/users?
+    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
      
      this.setState({users:res.data,loading:false}) //sending the data we got to the main state and reverting back loading to false after getting data 
+    }
+    //searching github users 
+    searchUsers= async text=>{
 
-    
-    
-  }
+      this.setState({loading:true})//setting loading true till we get back our data as per requirement 
+
+   const res = await axios
+    .get(`https://api.github.com/search/users?q=${text}
+    &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+     
+     this.setState({users: res.data.items ,loading:false})
+    }
+
+    //clearing users form state 
+    clearUsers=()=>{
+      this.setState({users:[],loading:false})
+    }
+
   render(){
     return (
       <div className="App">
         <Navbar title="github finder"/>
         <div className="container">
-          <Search/>
-          <Users loading={this.state.loading} users={this.state.users}></Users>
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers}/>
+          <Users loading={this.state.loading} users={this.state.users} ></Users>
           </div>
         
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react'
 
 class Search extends Component {
     state={
-        text:''
-    }
+        text:'',
+     
+    };
+    onSubmit=(e)=>{
+        e.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({text:''});
+    };
     onChange=(e)=>{
         this.setState({[e.target.name]:e.target.value})
-    }
+    };
   render() {
     return (
       <div>
-        <form  className="form">
+        <form  
+        onSubmit={this.onSubmit}
+         className="form">
             <input type="text" name="text" placeholder="Search User..." value={this.state.text} onChange={this.onChange} />
             <input type="submit" value="search" className="btn btn-dark btn-block"/>
+            
         </form>
+        <button className="btn btl-light btn-block" onClick={this.props.clearUsers}>Clear</button>
 
       </div>
     )
